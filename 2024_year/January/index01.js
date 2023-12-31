fcdd58fe47a71bbc2818e8dff4175d84c0b6d51a
Nimisha Nada <154672266+nimishanada@users.noreply.github.com>
2023-12-31 04:46:08 -0800

    find the area of a triangle

---
+++ 2024_year/January/index01.js
@@ -0,0 +1,13 @@
+// find the area of a triangle
+
+function areaOfTriangle(side1,side2,side3){
+    const s = (side1 + side2 + side3) / 2;
+    
+    
+    const areaValue = Math.sqrt(
+      s * (s - side1) * (s - side2) * (s - side3)
+    );
+    return areaValue;
+    }
+    
+    console.log(areaOfTriangle(3,4,5));  // 6
