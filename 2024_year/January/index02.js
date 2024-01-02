fffe560331e776d34901d3733dadaf2333e8f2da
Nimisha Nada <154672266+nimishanada@users.noreply.github.com>
2024-01-01 21:02:47 -0800

    check if the number is positive or negative

---
+++ 2024_year/January/index02.js
@@ -0,0 +1,16 @@
+// check if the number is positive or negative
+
+function getNumber(number){
+
+    if (number > 0) {
+    
+      console.log("The number is positive");
+    }
+    
+    else {
+    
+      console.log("The number is negative number");
+    }
+    
+    }
+    console.log(getNumber(-3)); // The number is negative number
