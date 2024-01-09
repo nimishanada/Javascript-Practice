3142ade63dad38976cfd8fe5c48def531777def5
Nimisha Nada <154672266+nimishanada@users.noreply.github.com>
2024-01-08 20:56:08 -0800

    check the number is even or odd

---
+++ 2024_year/January/index08.js
@@ -0,0 +1,15 @@
+//check the number is even or odd
+
+function myFunction(number){
+
+    if(number % 2 == 0) {
+
+        console.log("The number is even.");
+    }
+    
+    else {
+        console.log("The number is odd.");
+    }
+    
+    }
+    console.log(myFunction(5));
