2299366d559309d68938437361886668e82ac3b9
Nimisha Nada <154672266+nimishanada@users.noreply.github.com>
2024-01-09 21:14:27 -0800

    Break in switch case

---
+++ 2024_year/January/index09.js
@@ -0,0 +1,19 @@
+// Break in switch case
+
+let fruit = 'apple';
+
+switch(fruit) {
+
+    case 'apple':
+    case 'mango':
+    case 'pineapple':
+
+    console.log(`${fruit} is a fruit.`);
+        break;
+
+        default:
+
+        console.log(`${fruit} is not a fruit.`);
+        break;
+
+    } //apple is a fruit.
