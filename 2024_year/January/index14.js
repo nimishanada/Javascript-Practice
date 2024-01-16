ae8233b7e54d3715a2cc97a105782162f0660b86
Nimisha Nada <154672266+nimishanada@users.noreply.github.com>
2024-01-15 21:13:32 -0800

    convert celsius to fahrenheit

---
+++ 2024_year/January/index14.js
@@ -0,0 +1,13 @@
+//convert celsius to fahrenheit
+
+function convertCel(celsius){
+
+
+    const fahrenheit = (celsius * 1.8) + 32
+    
+    
+    return fahrenheit;
+    } 
+    console.log(convertCel(55)); //131
+    
+    
