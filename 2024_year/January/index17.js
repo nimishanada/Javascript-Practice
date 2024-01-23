1c37b7359dd4253ba95d5aeed55be86ad343a9d7
Nimisha Nada <154672266+nimishanada@users.noreply.github.com>
2024-01-22 20:00:01 -0800

    find square by array.map method

---
+++ 2024_year/January/index17.js
@@ -0,0 +1,7 @@
+// find square by array.map method
+
+let arr=[6,7,4,5]
+
+let newArray = arr.map(val => val * val)
+
+console.log(newArray); //36,49,36,25
---
+++ 2024_year/January/index18.js
@@ -0,0 +1,8 @@
+// removed element from array by arr.pop method 
+
+let cities = ["Madrid", "New York", "Kathmandu", "Paris"];
+
+let removedCity = cities.pop();
+
+console.log(removedCity);   // Paris
+    
