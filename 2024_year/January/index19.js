c2a0749c8e4e3ae4cec4f634ce6bdab8b6b53995
Nimisha Nada <154672266+nimishanada@users.noreply.github.com>
2024-01-24 19:56:47 -0800

    Add element by array.push method

---
+++ 2024_year/January/index19.js
@@ -0,0 +1,8 @@
+//Add element by array.push method
+
+const animals = ['pigs', 'goats', 'sheep'];
+
+const count = animals.push('cows');
+
+console.log(animals); // ["pigs", "goats", "sheep", "cows"]
+
