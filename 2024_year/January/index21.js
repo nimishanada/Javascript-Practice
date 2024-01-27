6d8bcc4f29c2cc37f54831c45d7499929ded4e60
Nimisha Nada <154672266+nimishanada@users.noreply.github.com>
2024-01-27 02:57:01 -0800

    Add element by array.unshift method

---
+++ 2024_year/January/index21.js
@@ -0,0 +1,7 @@
+//Add element by array.unshift method 
+
+const animals = ['pigs', 'goats', 'sheep','cow'];
+
+const count = animals.unshift('cat');
+
+console.log(animals); // ["cat", "pigs", "goats", "sheep", "cow"]
