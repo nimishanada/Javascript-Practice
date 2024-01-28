17fabcca0b32f07e1f67ad6ff3610446dd9b3f23
Nimisha Nada <154672266+nimishanada@users.noreply.github.com>
2024-01-27 20:24:07 -0800

    JavaScript code for concat() method

---
+++ 2024_year/January/index22.js
@@ -0,0 +1,13 @@
+// JavaScript code for concat() method
+
+function funConcat() {
+
+    let num1 = [11, 12, 13],
+        num2 = [14, 15, 16],
+        num3 = [17, 18, 19];
+
+    console.log(num1.concat(num2, num3));
+
+}
+
+funConcat(); //[11, 12, 13, 14, 15, 16, 17, 18, 19]
