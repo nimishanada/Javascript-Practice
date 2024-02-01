a5658ae0242021f99d1e52f80248007306ba6895
Nimisha Nada <154672266+nimishanada@users.noreply.github.com>
2024-02-01 08:45:14 -0800

    array.splice method

---
+++ 2024_year/January/February/index01.js
@@ -0,0 +1,6 @@
+//array.splice method
+let webDvlop = ["HTML", "CSS", "JS", "Bootstrap"];
+
+let removed = webDvlop.splice(2, 1, 'PHP', 'React_Native')
+
+console.log(webDvlop); //["HTML", "CSS", "PHP", "React_Native", "Bootstrap"]
